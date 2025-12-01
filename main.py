ec73a4fce42000db7dff46ca9d2119776bfd0bf1
2pac <tarasyaremabcn@gmail.com>
2025-12-01 17:59:52 +0100

    1 ok

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import asyncio
 import importlib
 import sys
+from time import time
 from pathlib import Path
 
 import click
+
+
+MAIN_TEMPLATE = '''
+import aiofiles
+
+async def main(fn: str = "in.txt", stage: int = 1):
+    async with aiofiles.open(fn, mode='r') as f:
+        f = await f.readlines()
+
+    print("{day}: {{}} -> {{}} lines".format(fn, len(f)))
+'''.strip()
 
 
 @click.group()
@@ -13,17 +25,37 @@ def main():
 
 @main.command()
 @click.argument("n", type=int)
-def run(n: int):
+@click.option("--fn", "-f", type=str, default="in.txt", help="Input file name", show_default=True)
+@click.option("--stage", "-s", type=int, default=0, help="Stage number (1 or 2)", show_default=True)
+def run(n: int, fn: str = "in.txt", stage: int = 1):
     day = f"{n:02d}"
     module_name = f"src.{day}.main"
 
     try:
         module = importlib.import_module(module_name)
+
     except ModuleNotFoundError:
         print(f"Error: Module {module_name} not found")
         sys.exit(1)
 
-    asyncio.run(module.main())
+    if stage == 1:
+        fn = "in1.txt"
+
+    elif stage == 2:
+        fn = "in2.txt"
+
+    # Make the path relative to the module
+    fn_p = Path(__file__).parent / "src" / day / fn
+
+    t = time()
+    asyncio.run(
+        module.main(
+            fn=fn_p.as_posix(),
+            stage=stage,
+        )
+    )
+
+    print(f"\n---\n{n:02d}.{stage}: {time() - t:.4f} s")
 
 
 @main.command()
@@ -38,9 +70,7 @@ def new(n: int):
 
     day_dir.mkdir(parents=True)
     (day_dir / "__init__.py").touch()
-    (day_dir / "main.py").write_text(f'''async def main():
-    print("Hello from aoc25 Day #{day}!")
-''')
+    (day_dir / "main.py").write_text(MAIN_TEMPLATE.format(day=day))
     (day_dir / "in.txt").touch()
     (day_dir / "in1.txt").touch()
     (day_dir / "in2.txt").touch()
--- src/01/main.py
+++ src/01/main.py
@@ -1,2 +1,61 @@
-async def main():
-    print("Hello from aoc25!")
+import aiofiles
+
+
+async def main(fn: str, stage: int = 1):
+    async with aiofiles.open(fn, mode='r') as f:
+        f = await f.readlines()
+
+    d = [
+        (x[0], int(x[1:]))
+        for x in f
+    ]
+
+    print(f"1: {await p1(d)}")
+    print(f"2: {await p2(d)}")
+
+
+async def p1(l: list[tuple[str, int]]):
+    p = 50
+    zero = 0
+
+    for d, v in l:
+        match d:
+            case "L":
+                p = (p - v) % 100
+
+            case "R":
+                p = (p + v) % 100
+
+        if p == 0:
+            zero += 1
+
+    return p, zero
+
+
+async def p2(l: list[tuple[str, int]]):
+    p = 50
+    zero = 0
+
+    for d, v in l:
+        pp = p
+
+        match d:
+            case "L":
+                p = p - v
+
+                if p <= 0:
+                    # The pp check is key for when you started at 0 :/
+                    # to not count it twice...
+                    zero += ((p * -1) // 100) + (1 if pp != 0 else 0)
+
+                p = p % 100
+
+            case "R":
+                p = p + v
+
+                if p >= 100:
+                    zero += p // 100
+
+                p = p % 100
+
+    return p, zero
